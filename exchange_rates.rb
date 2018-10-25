d09f4788ef14ac3ccaaffde4519d0320a5bb6662
Dobeb96 <dobosz.ma@interia.pl>
2018-10-25 10:25:43 +0200

    Class structure

---
+++ exchange_rates.rb
@@ -0,0 +1,20 @@
+class ExchangeRates
+  def initialize
+
+  end
+
+  private
+  def print_all_currencies
+
+  end
+
+  def print_extremum_rates
+
+  end
+
+  def save_to_file
+
+  end
+end
+
+ExchangeRates.new
