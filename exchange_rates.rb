e789d3b171168425a792d14827252e3d994eae46
Dobeb96 <dobosz.ma@interia.pl>
2018-10-25 11:13:15 +0200

    Print comma separated list of currencies

--- exchange_rates.rb
+++ exchange_rates.rb
@@ -7,11 +7,21 @@ class ExchangeRates
     uri = URI(EXCHANGE_API_URL)
     json_api = Net::HTTP.get(uri)
     @api = JSON.parse(json_api)
+
+    print_all_currencies(order: :desc)
   end
 
   private
-  def print_all_currencies
 
+  # OPTIONS: order: [:asc / :desc]
+  def print_all_currencies(options)
+    sorting = options[:order]
+
+    sorted_rates = @api['rates'].map {|k, _| k }.sort
+    sorted_rates.reverse! if sorting == :desc
+
+    puts 'Comma separated list of currencies:'
+    puts sorted_rates.join(', ')
   end
 
   def print_extremum_rates
